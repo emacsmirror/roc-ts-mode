f7d551b1a06ae8c414ff5fc93bf064e8895b812b
Tad Lispy <tadeusz@lazurski.pl>
2023-11-22 18:32:24 +0100

    Fix tree sitter highlighting for Roc
    Aligning with latest changes in the grammar.
    
    Looks like Git got really confused calculating diff here. The LSP code
    didn't really change.

--- config.el
+++ config.el
@@ -27,7 +27,6 @@
   (setq-local treesit-font-lock-feature-list
               '((basics)
                 (application-header)
-                (records)
                 (if-then-else)))
 
   (treesit-major-mode-setup))
@@ -39,7 +38,9 @@
     ((line_comment) @font-lock-comment-face
      (string) @font-lock-string-face
      (identifier) @font-lock-variable-use-face
+     (ident) @font-lock-variable-use-face ;; TODO: What's the difference?
      (module) @font-lock-constant-face
+     (field_name) @font-lock-property-name-face
      (expect "expect" @font-lock-keyword-face))
 
     :language roc
@@ -53,42 +54,32 @@
        (imports ("imports") @font-lock-keyword-face)
        (provides
         ("provides") @font-lock-keyword-face
-        (to "to") @font-lock-keyword-face))))
-
-    :language roc
-    :override t
-    :feature records
-    ((record
-      (record_field_expr (identifier)  @font-lock-property-name-face)))
+        (to) @font-lock-keyword-face))))
 
     :language roc
     :override t
     :feature if-then-else
     ((if_expression
-      ("if" @font-lock-keyword-face)
-      ("then" @font-lock-keyword-face))
-     (else_expression "else" @font-lock-keyword-face))
+      "if" @font-lock-keyword-face
+      (then ("then") @font-lock-keyword-face)
+      "else" @font-lock-keyword-face))
+
+    ;; Available Font Lock faces: https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
+    )
 
 
-
-     ;; Available Font Lock faces: https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
-    ))
-
-
-
-
-;; Setup the LSP support
-;; TODO: Is this the right way to set up the server? Am I hindering Doom's performance?
-;; See https://discourse.doomemacs.org/t/common-config-anti-patterns/119#loading-packages-too-early-3
-(if (and
-     (modulep! :tools lsp)
-     (modulep! :lang roc +lsp))
-    (use-package! lsp-mode
-      :config
-      (progn
-        (add-to-list 'lsp-language-id-configuration '(roc-mode . "roc"))
-        (lsp-register-client (make-lsp-client :new-connection (lsp-stdio-connection "roc_ls")
-                                              :activation-fn (lsp-activate-on "roc")
-                                              :major-modes '(roc-mode)
-                                              :server-id 'roc_ls))
-        (add-hook 'roc-mode-local-vars-hook #'lsp!))))
+  ;; Setup the LSP support
+  ;; TODO: Is this the right way to set up the server? Am I hindering Doom's performance?
+  ;; See https://discourse.doomemacs.org/t/common-config-anti-patterns/119#loading-packages-too-early-3
+  (if (and
+       (modulep! :tools lsp)
+       (modulep! :lang roc +lsp))
+      (use-package! lsp-mode
+        :config
+        (progn
+          (add-to-list 'lsp-language-id-configuration '(roc-mode . "roc"))
+          (lsp-register-client (make-lsp-client :new-connection (lsp-stdio-connection "roc_ls")
+                                                :activation-fn (lsp-activate-on "roc")
+                                                :major-modes '(roc-mode)
+                                                :server-id 'roc_ls))
+          (add-hook 'roc-mode-local-vars-hook #'lsp!)))))
