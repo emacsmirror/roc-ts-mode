47eb5a976fa5c2bc58f164407feb9fc20cd9656a
Tad Lispy <tadeusz@lazurski.pl>
2023-11-24 09:44:13 +0100

    Fix Roc mode for Emacs
    In recent commit I said LSP code was not modified. I was wrong :P

--- config.el
+++ config.el
@@ -65,21 +65,20 @@
       "else" @font-lock-keyword-face))
 
     ;; Available Font Lock faces: https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
-    )
+    ))
 
-
-  ;; Setup the LSP support
-  ;; TODO: Is this the right way to set up the server? Am I hindering Doom's performance?
-  ;; See https://discourse.doomemacs.org/t/common-config-anti-patterns/119#loading-packages-too-early-3
-  (if (and
-       (modulep! :tools lsp)
-       (modulep! :lang roc +lsp))
-      (use-package! lsp-mode
-        :config
-        (progn
-          (add-to-list 'lsp-language-id-configuration '(roc-mode . "roc"))
-          (lsp-register-client (make-lsp-client :new-connection (lsp-stdio-connection "roc_ls")
-                                                :activation-fn (lsp-activate-on "roc")
-                                                :major-modes '(roc-mode)
-                                                :server-id 'roc_ls))
-          (add-hook 'roc-mode-local-vars-hook #'lsp!)))))
+;; Setup the LSP support
+;; TODO: Is this the right way to set up the server? Am I hindering Doom's performance?
+;; See https://discourse.doomemacs.org/t/common-config-anti-patterns/119#loading-packages-too-early-3
+(if (and
+     (modulep! :tools lsp)
+     (modulep! :lang roc +lsp))
+    (use-package! lsp-mode
+      :config
+      (progn
+        (add-to-list 'lsp-language-id-configuration '(roc-mode . "roc"))
+        (lsp-register-client (make-lsp-client :new-connection (lsp-stdio-connection "roc_ls")
+                                              :activation-fn (lsp-activate-on "roc")
+                                              :major-modes '(roc-mode)
+                                              :server-id 'roc_ls))
+        (add-hook 'roc-mode-local-vars-hook #'lsp!))))
