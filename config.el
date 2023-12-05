83d6f7f860dff013fe7956234c9ced10b620eb2a
Tad Lispy <tadeusz@lazurski.pl>
2023-12-05 23:22:51 +0100

    Separate logic related to Roc Mode to own file
    It was part of my private Emacs Doom module, but the plan is to extract
    it into a package and publish it on MELPA or somewhere like that.

--- config.el
+++ config.el
@@ -1,73 +1,21 @@
 ;;; lang/roc/config.el -*- lexical-binding: t; -*-
 
-;; Setup the major mode for roc source files
-
-(define-derived-mode roc-mode prog-mode "Roc"
-  "Major mode for the Roc programming language"
-  (setq-local comment-start "#")
-  (when (treesit-ready-p 'roc)
-    (treesit-parser-create 'roc)
-    (roc-mode-ts-setup)))
+(load! "roc-mode.el")
+(require 'roc-mode)
 
 ;; TODO: Remove once I'm done working on the highlighting.
 (add-hook! 'roc-mode-hook #'treesit-inspect-mode)
 
-
-(add-to-list 'auto-mode-alist
-             '("\\.roc\\'" . roc-mode))
-
-(defun roc-mode-ts-setup ()
-  "Setup Tree Sitter for the Roc mode"
-
-  ;; TODO: There is a highlight.scm file in the tree-sitter-roc codebase. How can I use it?
-
-  (setq-local treesit-font-lock-settings
-              (apply #'treesit-font-lock-rules roc-ts-font-lock-rules))
-
-  (setq-local treesit-font-lock-feature-list
-              '((basics)
-                (application-header)
-                (if-then-else)))
-
-  (treesit-major-mode-setup))
-
-(defvar roc-ts-font-lock-rules
-  '(:language roc
-    :override t
-    :feature basics
-    ((line_comment) @font-lock-comment-face
-     (string) @font-lock-string-face
-     (identifier) @font-lock-variable-use-face
-     (ident) @font-lock-variable-use-face ;; TODO: What's the difference?
-     (module) @font-lock-constant-face
-     (field_name) @font-lock-property-name-face
-     (expect "expect" @font-lock-keyword-face))
-
-    :language roc
-    :override t
-    :feature application-header
-    ((app_header
-      "app" @font-lock-keyword-face
-      (app_name) @font-lock-string-face
-      (app_header_body
-       (packages ("packages") @font-lock-keyword-face)
-       (imports ("imports") @font-lock-keyword-face)
-       (provides
-        ("provides") @font-lock-keyword-face
-        (to) @font-lock-keyword-face))))
-
-    :language roc
-    :override t
-    :feature if-then-else
-    ((if_expression
-      "if" @font-lock-keyword-face
-      (then ("then") @font-lock-keyword-face)
-      "else" @font-lock-keyword-face))
-
-    ;; Available Font Lock faces: https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
-    ))
-
 ;; Setup the LSP support
+
+(defun roc-mode-lsp-setup ()
+  "Setup LSP for Roc. Requires lsp-mode package."
+  (add-to-list 'lsp-language-id-configuration '(roc-mode . "roc"))
+  (lsp-register-client (make-lsp-client :new-connection (lsp-stdio-connection "roc_ls")
+                                        :activation-fn (lsp-activate-on "roc")
+                                        :major-modes '(roc-mode)
+                                        :server-id 'roc_ls)))
+
 ;; TODO: Is this the right way to set up the server? Am I hindering Doom's performance?
 ;; See https://discourse.doomemacs.org/t/common-config-anti-patterns/119#loading-packages-too-early-3
 (if (and
@@ -76,9 +24,5 @@
     (use-package! lsp-mode
       :config
       (progn
-        (add-to-list 'lsp-language-id-configuration '(roc-mode . "roc"))
-        (lsp-register-client (make-lsp-client :new-connection (lsp-stdio-connection "roc_ls")
-                                              :activation-fn (lsp-activate-on "roc")
-                                              :major-modes '(roc-mode)
-                                              :server-id 'roc_ls))
+        (roc-mode-lsp-setup)
         (add-hook 'roc-mode-local-vars-hook #'lsp!))))
---
+++ roc-mode.el
@@ -0,0 +1,106 @@
+;;; lang/roc/roc-mode.el -*- lexical-binding: t; -*-
+
+;;; roc-mode.el --- Roc programming language mode -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Tad Lispy
+;;
+;; Author: Tad Lispy <tadeusz@lazurski.pl>
+;; Maintainer: Tad Lispy <tadeusz@lazurski.pl>
+;; Created: November 30, 2023
+;; Version: 0.1-pre
+;; Keywords: languages
+;; Homepage: https://gitlab.com/tad-lispy/roc-mode
+;; Package-Requires: ((emacs "29.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;;  Roc is a strongly typed functional programming language inspired by Elm, but
+;;  for variety of platforms. See https://www.roc-lang.org/.
+;;
+;;; Code:
+
+
+(require 'treesit)
+
+;;;;; Commands
+
+(define-derived-mode roc-mode prog-mode "Roc"
+  "Major mode for the Roc programming language"
+  (setq-local comment-start "#")
+  (when (treesit-ready-p 'roc)
+    (treesit-parser-create 'roc)
+    (roc-mode--ts-setup)))
+
+(add-to-list 'auto-mode-alist
+             '("\\.roc\\'" . roc-mode))
+
+;;;; Private
+
+(defvar roc-mode--ts-font-lock-rules
+  '(:language roc
+    :override t
+    :feature basics
+    ((line_comment) @font-lock-comment-face
+     (string) @font-lock-string-face
+     (identifier) @font-lock-variable-use-face
+     (ident) @font-lock-variable-use-face ;; TODO: What's the difference?
+     (module) @font-lock-constant-face
+     (field_name) @font-lock-property-name-face
+     (expect "expect" @font-lock-keyword-face))
+
+    :language roc
+    :override t
+    :feature application-header
+    ((app_header
+      "app" @font-lock-keyword-face
+      (app_name) @font-lock-string-face
+      (app_header_body
+       (packages ("packages") @font-lock-keyword-face)
+       (imports ("imports") @font-lock-keyword-face)
+       (provides
+        ("provides") @font-lock-keyword-face
+        (to) @font-lock-keyword-face))))
+
+    :language roc
+    :override t
+    :feature if-then-else
+    ((if_expression
+      "if" @font-lock-keyword-face
+      (then ("then") @font-lock-keyword-face)
+      "else" @font-lock-keyword-face))))
+
+(defun roc-mode--ts-setup ()
+  "Setup Tree Sitter for the Roc mode"
+
+  ;; TODO: There is a highlight.scm file in the tree-sitter-roc codebase. How can I use it?
+
+  (setq-local treesit-font-lock-settings
+              (apply #'treesit-font-lock-rules roc-mode--ts-font-lock-rules))
+
+  (setq-local treesit-font-lock-feature-list
+              '((basics)
+                (application-header)
+                (if-then-else)))
+
+  (treesit-major-mode-setup))
+
+;;;; Footer
+
+(provide 'roc-mode)
+
+;;; roc-mode.el ends here
