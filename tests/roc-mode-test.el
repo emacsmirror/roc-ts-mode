0e4bffbed5439a29f74f3e468e22777c152c6a9e
Ajai Nelson <22969541+AjaiKN@users.noreply.github.com>
2024-05-27 14:57:59 -0400

    Test indent consistency with roc format command

--- tests/roc-mode-test.el
+++ tests/roc-mode-test.el
@@ -4,7 +4,23 @@
 (require 'roc-mode)
 
 (ert-deftest indent-examples ()
-  (ert-test-erts-file "./roc-mode-examples.erts"))
+  "Check that roc-mode indentation works correctly."
+  (ert-test-erts-file "./roc-mode-examples.erts"
+                      (lambda ()
+                        (roc-mode)
+                        (indent-region (point-min) (point-max)))))
+
+(ert-deftest confirm-roc-format ()
+  "Check that the roc format command produces the same result.
+
+This is not a test of roc-mode itself; it's just testing that our
+indentation examples in ./roc-mode-examples.erts are still
+up-to-date with the output of Roc's formatter."
+  (ert-test-erts-file "./roc-mode-examples.erts"
+                      (lambda ()
+                        (shell-command-on-region (point-min) (point-max)
+                                                 "roc format --stdin --stdout"
+                                                 (current-buffer) t))))
 
 (provide 'roc-mode-test)
 ;;; roc-mode-test.el ends here
